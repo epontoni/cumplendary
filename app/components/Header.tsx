3b456722b8c7353a40688369980e8905b491e3f2
Emanuel Pontoni <pontoni.emanuel@gmail.com>
2025-05-18 16:20:33 -0300

    first commit

---
+++ app/components/Header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <header>
+            <h1 className="leading-none text-xl">
+                <span className="font-yellowtail text-2xl">Calendario de</span><span>🎈</span><br />
+                <span className="uppercase font-poetsenone text-blue-500">cumpleaños</span>
+            </h1>
+      </header>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,188 @@
-import Image from "next/image";
+import { Cake } from "lucide-react";
+import Header from "./components/Header";
+
+const calendar = [
+  {
+    month: "Enero",
+    birthdays: [
+      {
+        name: "Cumpleañero 1",
+        date: new Date(),
+      },
+      {
+        name: "Cumpleañero 2",
+        date: new Date(),
+      },
+    ],
+  },
+
+  {
+    month: "Febrero",
+    birthdays: [
+      {
+        name: "Cumpleañero 3",
+        date: new Date(),
+      },
+    ],
+  },
+];
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <>
+      <div className="p-1 flex flex-col gap-2 items-center justify-center bg-gray-100 absolute top-0 z-[-2] h-screen w-screen bg-[radial-gradient(100%_50%_at_50%_0%,rgba(0,163,255,0.13)_0,rgba(0,163,255,0)_50%,rgba(0,163,255,0)_100%)]">
+        <Header />
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <section className="grid lg:grid-cols-6 grid-cols-3 p-8">
+          <div className="bg-white rounded-lg shadow p-6 flex items-center justify-center">
+            <div className="flex flex-col gap-2 items-center">
+              <div className="text-4xl font-bold text-blue-500">Cumpleaños</div>
+              <div className="text-sm font-light text-gray-500">
+                ¡Celebra con nosotros!
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className="bg-gray-100 p-2 w-full rounded-md">
+          <div className="flex gap-2 items-center bg-white px-2">
+            <div className="vertical-rl font-poetsenone text-center">ENERO</div>
+
+            <div className="flex flex-col flex-1 gap-1">
+              <div className="flex flex-1 gap-2 items-center bg-white px-2">
+                <div>
+                  <Cake className="text-blue-500" />
+                </div>
+                <div className="flex flex-1 justify-between items-center">
+                  <div>
+                    <div className="text-sm font-bold">APELLIDO, Nombre</div>
+                    <div className="text-xs">9 de Agosto</div>
+                  </div>
+                  <div className="text-sm font-light">en 19 días</div>
+                </div>
+              </div>
+
+              <div className="flex flex-1 gap-2 items-center bg-white px-2">
+                <div>
+                  <Cake className="text-blue-500" />
+                </div>
+                <div className="flex flex-1 justify-between items-center">
+                  <div>
+                    <div className="text-sm font-bold">APELLIDO, Nombre</div>
+                    <div className="text-xs">9 de Agosto</div>
+                  </div>
+                  <div className="text-sm font-light">en 19 días</div>
+                </div>
+              </div>
+
+              <div className="flex flex-1 gap-2 items-center bg-white px-2">
+                <div>
+                  <Cake className="text-blue-500" />
+                </div>
+                <div className="flex flex-1 justify-between items-center">
+                  <div>
+                    <div className="text-sm font-bold">APELLIDO, Nombre</div>
+                    <div className="text-xs">9 de Agosto</div>
+                  </div>
+                  <div className="text-sm font-light">en 19 días</div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex gap-2 items-center bg-white px-2">
+            <div className="vertical-rl font-poetsenone text-center">
+              FEBRERO
+            </div>
+
+            <div className="flex flex-col flex-1 gap-1">
+              <div className="flex flex-1 gap-2 items-center bg-white px-2">
+                <div>
+                  <Cake className="text-blue-500" />
+                </div>
+                <div className="flex flex-1 justify-between items-center">
+                  <div>
+                    <div className="text-sm font-bold">APELLIDO, Nombre</div>
+                    <div className="text-xs">9 de Agosto</div>
+                  </div>
+                  <div className="text-sm font-light">en 19 días</div>
+                </div>
+              </div>
+
+              <div className="flex flex-1 gap-2 items-center bg-white px-2">
+                <div>
+                  <Cake className="text-blue-500" />
+                </div>
+                <div className="flex flex-1 justify-between items-center">
+                  <div>
+                    <div className="text-sm font-bold">APELLIDO, Nombre</div>
+                    <div className="text-xs">9 de Agosto</div>
+                  </div>
+                  <div className="text-sm font-light">en 19 días</div>
+                </div>
+              </div>
+
+              <div className="flex flex-1 gap-2 items-center bg-white px-2">
+                <div>
+                  <Cake className="text-blue-500" />
+                </div>
+                <div className="flex flex-1 justify-between items-center">
+                  <div>
+                    <div className="text-sm font-bold">APELLIDO, Nombre</div>
+                    <div className="text-xs">9 de Agosto</div>
+                  </div>
+                  <div className="text-sm font-light">en 19 días</div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex gap-2 items-center bg-white px-2">
+            <div className="vertical-rl font-poetsenone text-center">MARZO</div>
+
+            <div className="flex flex-col flex-1 gap-1">
+              <div className="flex flex-1 gap-2 items-center bg-white px-2">
+                <div>
+                  <Cake className="text-blue-500" />
+                </div>
+                <div className="flex flex-1 justify-between items-center">
+                  <div>
+                    <div className="text-sm font-bold">APELLIDO, Nombre</div>
+                    <div className="text-xs">9 de Agosto</div>
+                  </div>
+                  <div className="text-sm font-light">en 19 días</div>
+                </div>
+              </div>
+
+              <div className="flex flex-1 gap-2 items-center bg-white px-2">
+                <div>
+                  <Cake className="text-blue-500" />
+                </div>
+                <div className="flex flex-1 justify-between items-center">
+                  <div>
+                    <div className="text-sm font-bold">APELLIDO, Nombre</div>
+                    <div className="text-xs">9 de Agosto</div>
+                  </div>
+                  <div className="text-sm font-light">en 19 días</div>
+                </div>
+              </div>
+
+              <div className="flex flex-1 gap-2 items-center bg-white px-2">
+                <div>
+                  <Cake className="text-blue-500" />
+                </div>
+                <div className="flex flex-1 justify-between items-center">
+                  <div>
+                    <div className="text-sm font-bold">APELLIDO, Nombre</div>
+                    <div className="text-xs">9 de Agosto</div>
+                  </div>
+                  <div className="text-sm font-light">en 19 días</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </>
   );
 }
